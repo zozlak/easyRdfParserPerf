2ada64d06d0d36da41252be3f33f40b497e0a0f6
Mateusz Żółtak <zozlak@zozlak.org>
2020-12-29 20:17:42 +0100

    Old EasyRdf releases testing added

---
+++ testOldReleases.php
@@ -0,0 +1,72 @@
+#!/usr/bin/php
+<?php
+/*
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2020, Mateusz Żółtak
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+if ($argc !== 1 && !preg_match('/^[0-9][.][0-9][.][0-9]$/', $argv[1] ?? '')) {
+    echo "$argv[0] [release]
+
+Runs test.php on old EasyRdf releases
+        
+- Only releases >=1.0.0 are supported.
+- If no release number is supported, all releases >= 1.0.0. are tested
+";
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+const MIN_VERSION = '1.0.0';
+const REPO_URL = 'https://api.github.com/repos/easyrdf/easyrdf/releases';
+
+$test = new EasyRdf\ParserPerfTest\Test();
+
+if ($argc === 2) {
+    $versions = [$argv[1]];
+} else {
+    $client   = new GuzzleHttp\Client();
+    $headers  = ['Accept' => 'application/vnd.github.v3+json'];
+    $request  = new GuzzleHttp\Psr7\Request('get', REPO_URL, $headers);
+    $response = $client->sendRequest($request);
+    $releases = json_decode($response->getBody());
+    $versions = [];
+    foreach ($releases as $n => $i) {
+        if ($n > 0 && $i->tag_name >= MIN_VERSION) {
+            $versions[] = $i->tag_name;
+        } elseif ($n > 0) {
+            break;
+        }
+    }
+}
+
+foreach ($versions as $version) {
+    echo "# $version\n";
+    echo shell_exec(sprintf("composer require easyrdf/easyrdf:%s >/dev/null 2>&1 && php -f test.php", $version)) . "\n";
+}
