a56173ac4729e9e39680e15a1f48033b2eaa926b
Mateusz Żółtak <zozlak@zozlak.org>
2020-12-30 17:33:17 +0100

    cache R packages

--- plots.R
+++ plots.R
@@ -1,4 +1,4 @@
-localLib = paste0(getwd(), '/R')
+localLib = path.expand('~/R')
 dir.create(localLib)
 .libPaths(c(localLib, .libPaths()))
 install.packages(setdiff(c('dplyr', 'jsonlite', 'ggplot2', 'magrittr', 'svglite'), installed.packages()[, 'Package']), localLib)
