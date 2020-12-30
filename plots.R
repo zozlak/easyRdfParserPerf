cfff83f2f9ac8b7b5a5f903f4f53b7616777d7e9
Mateusz Żółtak <zozlak@zozlak.org>
2020-12-30 18:12:25 +0100

    restore big tests and adjust plot type for single tests

--- plots.R
+++ plots.R
@@ -12,21 +12,33 @@ data = jsonlite::read_json('output/data.json', simplifyVector = TRUE) %>%
     triplesPerSecond = .data$triplesCount / .data$time,
     test = paste(.data$class, .data$file, sep = '\n')
   )
+nmax = data %>% 
+  dplyr::group_by(.data$class, .data$dataFile) %>% 
+  dplyr::filter(!is.na(.data$time)) %>% 
+  dplyr::summarize(n = dplyr::n()) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::summarise(n = max(n)) %>% 
+  unlist()
+if (nmax > 1) {
+  plotfun = ggplot2::geom_violin
+} else {
+  plotfun = ggplot2::geom_point
+}
 
 plot = data %>% ggplot2::ggplot(ggplot2::aes(x = test, y = time, color = class)) +
-  ggplot2::geom_violin() +
+  plotfun() +
   ggplot2::ggtitle('Parsing time') +
   ggplot2::facet_wrap(dplyr::vars(format), ncol = 1) +
   ggplot2::theme_light()
 ggplot2::ggsave('output/parsing_time.svg', plot, 'svg', width = 10, height = 10)
 plot = data %>% ggplot2::ggplot(ggplot2::aes(x = test, y = triplesPerSecond, color = class)) +
-  ggplot2::geom_violin() +
+  plotfun() +
   ggplot2::ggtitle('Throughput') +
   ggplot2::facet_wrap(dplyr::vars(format), ncol = 1) +
   ggplot2::theme_light()
 ggplot2::ggsave('output/troughput.svg', plot, 'svg', width = 10, height = 10)
 plot = data %>% ggplot2::ggplot(ggplot2::aes(x = test, y = memoryMb, color = class)) +
-  ggplot2::geom_violin() +
+  plotfun +
   ggplot2::ggtitle('Memory usage') +
   ggplot2::facet_wrap(dplyr::vars(format), ncol = 1) +
   ggplot2::theme_light()
